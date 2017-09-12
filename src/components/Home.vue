29f1dfc3580f706bd0ec84f311af74a6c19cc792
Anthony Nandaa <profnandaa@gmail.com>
2017-09-13 02:23:44 +0300

    initial setup

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="hero is-small">
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Admin Dashboard
+        </h1>
+        <h2 class="subtitle">Workplace Incidence Reporting Platform</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+h2.subtitle {
+  color: #888;
+  font-size: small;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
